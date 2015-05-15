2db2df1400dba1b353346af0196a90140f81c3a7
davidbarbeiro <david.gdbarbeiro@gmail.com>
2015-05-15 17:19:49 +0100

    input validation

--- InsulinInterface/src/server/InsulinDoseCalculator.java
+++ InsulinInterface/src/server/InsulinDoseCalculator.java
@@ -1,55 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package server;
-
 import javax.jws.WebService;
-import org.apache.commons.math.stat.regression.SimpleRegression;
+import javax.jws.WebMethod;
 
-@WebService(endpointInterface = "server.InsulinDoseCalculatorInterface")
-public class InsulinDoseCalculator implements InsulinDoseCalculatorInterface {
-
-    @Override
-    public int mealtimeInsulinDose(int carbohydrateAmount, int carbohydrateToInsulinRatio, int preMealBloodSugar, int targetBloodSugar, int personalSensitivity) {
-        try {
-            if (targetBloodSugar > preMealBloodSugar) {
-                return 0;
-            }
-            int carbohydrateDose =(int) Math.round((double)carbohydrateAmount / carbohydrateToInsulinRatio / personalSensitivity *50);
-            int highBloodSugarDose = (int) Math.round((double)(preMealBloodSugar - targetBloodSugar) / personalSensitivity);
-            return carbohydrateDose + highBloodSugarDose;
-        } catch (Exception e) {
-            return -1;
-        }
-    }
-
-    @Override
-    public int backgroundInsulinDose(int bodyWeight) {
-        try {
-            return (int) Math.round(0.5 * 0.55 * bodyWeight);
-        } catch (Exception e) {
-            return -1;
-        }
-    }
-
-    @Override
-    public int personalSensitivityToInsulin(int physicalActivityLevel, int[] physicalActivitySamples, int[] bloodSugarDropSamples) {
-        try {
-            if(physicalActivitySamples.length<2 || (physicalActivitySamples.length!=bloodSugarDropSamples.length) ){
-                return -1;
-            }
-            SimpleRegression regression = new SimpleRegression();
-            for(int i=0;i<physicalActivitySamples.length;i++){
-                regression.addData(physicalActivitySamples[i], bloodSugarDropSamples[i]);
-            }
-            double alpha=regression.getIntercept();
-            double beta = regression.getSlope();
-            return (int) Math.round(alpha+beta*physicalActivityLevel);
-        } catch (Exception e) {
-            return -1;
-        }
-    }
+@WebService
+public interface InsulinDoseCalculatorInterface {
+    @WebMethod
+    int mealtimeInsulinDose(int carbohydrateAmount,
+                      int carbohydrateToInsulinRatio,
+                      int preMealBloodSugar,
+                      int targetBloodSugar,
+                      int personalSensitivity);
+    
+    @WebMethod
+    int backgroundInsulinDose(int bodyWeight);
+    
+    @WebMethod
+    int personalSensitivityToInsulin(int physicalActivityLevel,
+                               int[] physicalActivitySamples,
+                               int[] bloodSugarDropSamples);
 
 }
